a86ae7935fb75ea708b8027428d56931a30f92dc
Wilson Pinto <wilsonpinto88@gmail.com>
2023-01-26 14:41:18 +0000

    Add project files.

---
+++ Clean.Site/Components/NavigationViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Clean.Site.Models;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace Clean.Site.Components;
+
+[ViewComponent(Name = "Navigation")]
+public class NavigationViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(IPublishedContent content)
+    {
+        var homePage = content.AncestorOrSelf<HomePage>();
+        var navigationItems = homePage?.MainNavigation ?? Enumerable.Empty<NavigationItem>();
+
+        var model = new NavigationViewModel()
+        {
+            HomeUrl = homePage.Url(),
+            NavigationItems = navigationItems
+        };
+
+        return View(model);
+    }
+}
---
+++ Clean.Site/Models/NavigationViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace Clean.Site.Models
+{
+    public class NavigationViewModel
+    {
+        public string HomeUrl { get; set; }
+        public IEnumerable<NavigationItem> NavigationItems { get; set; }
+    }
+}
